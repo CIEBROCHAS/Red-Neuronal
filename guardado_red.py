ae620fb4fc3476b153cab70678262e7e6c6feb43
CIEBROCHAS <molano.juan.2629@eam.edu.co>
2025-06-08 11:22:34 -0500

    Añade funciones para el guardado y cargado de los pesos

---
+++ guardado_red.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Guarda el diccionario de pesos al archivo "pesos.npz"
+# ADVERTENCIA, lo que sea que se guarde sobreescribe lo que ya había ahí
+def guardar_pesos(dict_pesos) -> None:
+    dict_pesos_str = {str(k): v for k, v in dict_pesos.items()}  # Las llaves tienen que ser str
+    np.savez_compressed('pesos.npz', **dict_pesos_str)
+
+# Retorna un dict que contiene las matrices de pesos del archivo "pesos.npz"
+def cargar_pesos() -> dict:
+    np_data = np.load('pesos.npz')
+    dict_pesos = {eval(k): np_data[k] for k in np_data}
+    return dict_pesos
