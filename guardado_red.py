cf4ab730c14a3ad78e7a59cd3acd1d8287aaf92a
CIEBROCHAS <molano.juan.2629@eam.edu.co>
2025-06-08 11:47:39 -0500

    Modifica las funciones de persistencia para poder elegir la ruta. Añade gitignore a root. Quita pycache del git.

--- guardado_red.py
+++ guardado_red.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-# Guarda el diccionario de pesos al archivo "pesos.npz"
+# Guarda el diccionario de pesos al archivo en 'ruta', ejemplo: pesos.npz
 # ADVERTENCIA, lo que sea que se guarde sobreescribe lo que ya había ahí
-def guardar_pesos(dict_pesos) -> None:
+def guardar_pesos(dict_pesos, ruta) -> None:
     dict_pesos_str = {str(k): v for k, v in dict_pesos.items()}  # Las llaves tienen que ser str
-    np.savez_compressed('pesos.npz', **dict_pesos_str)
+    np.savez_compressed(ruta, **dict_pesos_str)
 
-# Retorna un dict que contiene las matrices de pesos del archivo "pesos.npz"
-def cargar_pesos() -> dict:
-    np_data = np.load('pesos.npz')
+# Retorna un dict que contiene las matrices de pesos del archivo en 'ruta', ej: pesos.npz
+def cargar_pesos(ruta) -> dict:
+    np_data = np.load(ruta)
     dict_pesos = {eval(k): np_data[k] for k in np_data}
     return dict_pesos
